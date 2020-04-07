a6f1b7f37be5b9477d2fe46a8c7975454d12e7b4
Gordiig <gordiig@gmail.com>
2020-04-07 16:50:59 +0300

    Rewrote AwardsRequester for new base methods

--- Awards/_AwardsRequester.py
+++ Awards/_AwardsRequester.py
@@ -3,7 +3,6 @@ from enum import Enum
 from typing import Tuple, List, Dict, Any, Union
 from django.conf import settings
 from ..BaseApiRequester import BaseApiRequester
-from ..exceptions import JsonDecodeError, UnexpectedResponse, RequestError
 
 
 class AwardsRequester(BaseApiRequester):
@@ -23,52 +22,36 @@ class AwardsRequester(BaseApiRequester):
         """
         Получение списка ачивок
         """
-        headers = self._create_auth_header_dict(token)
         params = {self.deleted_qparam: with_deleted}
-        response = self.get(path_suffix='achievements/', headers=headers, params=params)
-        self._validate_return_code(response, 200)
-        a_json = self.get_json_from_response(response)
-        return response, a_json
+        return self._base_get(token=token, path_suffix=self.achievement_suffix, params=params)
 
     def get_achievements_paginated(self, limit: int, offset: int, token: str, with_deleted: bool = False) -> \
             Tuple[requests.Response, Dict[str, Any]]:
         """
         Получение пагинированного списка ачивок
         """
-        headers = self._create_auth_header_dict(token)
         params = {'limit': limit, 'offset': offset, self.deleted_qparam: with_deleted}
-        response = self.get(path_suffix=self.achievement_suffix, headers=headers, params=params)
-        self._validate_return_code(response, 200)
-        p_json = self.get_json_from_response(response)
-        return response, p_json
+        return self._base_get(token=token, path_suffix=self.achievement_suffix, params=params)
 
     def get_achievement(self, achievement_id: int, token: str, with_deleted: bool = False) -> \
             Tuple[requests.Response, Dict[str, Any]]:
         """
         Получение определенной ачивки
         """
-        headers = self._create_auth_header_dict(token)
         params = {self.deleted_qparam: with_deleted}
-        response = self.get(path_suffix=f'{self.achievement_suffix}{achievement_id}/', headers=headers, params=params)
-        self._validate_return_code(response, 200)
-        a_json = self.get_json_from_response(response)
-        return response, a_json
+        return self._base_get(token=token, path_suffix=f'{self.achievement_suffix}{achievement_id}/', params=params)
 
     def create_achievement(self, name: str, token: str, descr: Union[str, None] = None,
                            pic_link: [str, None] = None) -> Tuple[requests.Response, Dict[str, Any]]:
         """
         Создание ачивки
         """
-        headers = self._create_auth_header_dict(token)
         data = {'name': name}
         if descr:
             data['descr'] = descr
         if pic_link:
             data['pic_link'] = pic_link
-        response = self.post(path_suffix=self.achievement_suffix, headers=headers, data=data)
-        self._validate_return_code(response, 201)
-        a_json = self.get_json_from_response(response)
-        return response, a_json
+        return self._base_post(token=token, path_suffix=self.achievement_suffix, data=data)
 
     def change_achievement(self, achievement_id: int, token: str, name: Union[str, None] = None,
                            descr: Union[str, None] = None, pic_link: Union[str, None] = None) -> \
@@ -76,7 +59,6 @@ class AwardsRequester(BaseApiRequester):
         """
         Изменение ачивки
         """
-        headers = self._create_auth_header_dict(token)
         data = dict()
         if name:
             data['name'] = name
@@ -84,19 +66,13 @@ class AwardsRequester(BaseApiRequester):
             data['descr'] = descr
         if pic_link:
             data['pic_link'] = pic_link
-        response = self.patch(path_suffix=f'{self.achievement_suffix}{achievement_id}/', headers=headers, data=data)
-        self._validate_return_code(response, 202)
-        a_json = self.get_json_from_response(response)
-        return response, a_json
+        return self._base_patch(path_suffix=f'{self.achievement_suffix}{achievement_id}/', token=token, data=data)
 
     def delete_achievement(self, achievement_id: int, token: str) -> requests.Response:
         """
         Удаление ачивки
         """
-        headers = self._create_auth_header_dict(token)
-        response = self.delete(path_suffix=f'{self.achievement_suffix}{achievement_id}/', headers=headers)
-        self._validate_return_code(response, 204)
-        return response
+        return self._base_delete(path_suffix=f'{self.achievement_suffix}{achievement_id}/', token=token)
 
     # MARK: - Pins
 
@@ -109,55 +85,39 @@ class AwardsRequester(BaseApiRequester):
         """
         Получение пинов
         """
-        headers = self._create_auth_header_dict(token)
         params = {self.deleted_qparam: with_deleted}
         if pin_type:
             params[self.pin_type_qparam] = pin_type.value
-        response = self.get(path_suffix=self.pins_suffix, headers=headers, params=params)
-        self._validate_return_code(response, 200)
-        p_json = self.get_json_from_response(response)
-        return response, p_json
+        return self._base_get(path_suffix=self.pins_suffix, token=token, params=params)
 
     def get_pins_paginated(self, limit: int, offset: int, token: str, pin_type: Union[PIN_TYPE, None] = None,
                            with_deleted: bool = False) -> Tuple[requests.Response, Dict[str, Any]]:
         """
         Получение пагинированных пинов
         """
-        headers = self._create_auth_header_dict(token)
         params = {self.deleted_qparam: with_deleted, 'limit': limit, 'offset': offset}
         if pin_type:
             params[self.pin_type_qparam] = pin_type.value
-        response = self.get(path_suffix=self.pins_suffix, headers=headers, params=params)
-        self._validate_return_code(response, 200)
-        p_json = self.get_json_from_response(response)
-        return response, p_json
+        return self._base_get(path_suffix=self.pins_suffix, token=token, params=params)
 
     def get_pin(self, pin_id: int, token: str, with_deleted: bool = False) -> Tuple[requests.Response, Dict[str, Any]]:
         """
         Получение пина
         """
-        headers = self._create_auth_header_dict(token)
         params = {self.deleted_qparam: with_deleted}
-        response = self.get(path_suffix=f'{self.pins_suffix}{pin_id}/', headers=headers, params=params)
-        self._validate_return_code(response, 200)
-        p_json = self.get_json_from_response(response)
-        return response, p_json
+        return self._base_get(path_suffix=f'{self.pins_suffix}{pin_id}/', token=token, params=params)
 
     def create_pin(self, name: str, ptype: PIN_TYPE, price: int, token: str, descr: Union[str, None] = None,
                    pic_link: Union[str, None] = None) -> Tuple[requests.Response, Dict[str, Any]]:
         """
         Создание пина
         """
-        headers = self._create_auth_header_dict(token)
         data = {'name': name, 'ptype': ptype.value, 'price': price}
         if descr:
             data['descr'] = descr
         if pic_link:
             data['pic_link'] = pic_link
-        response = self.post(path_suffix=self.pins_suffix, headers=headers, data=data)
-        self._validate_return_code(response, 201)
-        p_json = self.get_json_from_response(response)
-        return response, p_json
+        return self._base_post(path_suffix=self.pins_suffix, token=token, data=data)
 
     def change_pin(self, pin_id: int, token: str, name: Union[str, None] = None, price: Union[int, None] = None,
                    descr: Union[str, None] = None, pic_link: Union[str, None] = None) -> \
@@ -165,7 +125,6 @@ class AwardsRequester(BaseApiRequester):
         """
         Изменение пина
         """
-        headers = self._create_auth_header_dict(token)
         data = dict()
         if name:
             data['name'] = name
@@ -175,16 +134,10 @@ class AwardsRequester(BaseApiRequester):
             data['descr'] = descr
         if pic_link:
             data['pic_link'] = pic_link
-        response = self.patch(path_suffix=f'{self.pins_suffix}{pin_id}/', headers=headers, data=data)
-        self._validate_return_code(response, 202)
-        p_json = self.get_json_from_response(response)
-        return response, p_json
+        return self._base_patch(path_suffix=f'{self.pins_suffix}{pin_id}/', token=token, data=data)
 
     def delete_pin(self, pin_id: int, token: str) -> requests.Response:
         """
         Удаление пина
         """
-        headers = self._create_auth_header_dict(token)
-        response = self.delete(path_suffix=f'{self.achievement_suffix}{pin_id}/', headers=headers)
-        self._validate_return_code(response, 204)
-        return response
+        return self._base_delete(path_suffix=f'{self.achievement_suffix}{pin_id}/', token=token)

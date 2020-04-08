896999333235d36c41a4293d8c383f980f7501cf
Gordiig <gordiig@gmail.com>
2020-04-08 19:16:03 +0300

    Finished with MockMediaRequester

--- Media/MediaRequester.py
+++ Media/MediaRequester.py
@@ -1,4 +1,15 @@
+from django.conf import settings
 from ._MediaRequester import MediaRequester as __me
+from ._MockMediaRequester import MockMediaRequester as __m
 
 
-MediaRequester = __me
+__tst = settings.TESTING
+try:
+    if __tst:
+        __art = settings.ALLOW_REQUESTS_TEST
+        MediaRequester = __me if __art else __m
+    else:
+        __ar = settings.ALLOW_REQUESTS
+        MediaRequester = __me if __ar else __m
+except AttributeError:
+    MediaRequester = __me if not __tst else __m
---
+++ Media/_MockMediaRequester.py
@@ -0,0 +1,45 @@
+import requests
+from typing import Tuple, List, Dict, Any, Union
+from ._MediaRequester import MediaRequester
+from ..Mock.MockRequesterMixin import MockRequesterMixin
+
+
+class MockMediaRequester(MediaRequester, MockRequesterMixin):
+    """
+    Мок-реквестер для медии
+    """
+    # MARK: - Mock overrides
+    def get_mine_error_part(self, token):
+        return self.get_media_error_part(token)
+
+    def get_object_on_success(self, token=None):
+        return {
+            'id': 1,
+            'object_type': 'user',
+            'object_id': 1,
+            'image_url': '/media/testcat.jpg/',
+            'created_by': 1,
+            'created_dt': '2012-03-03T12:12:12Z',
+            'deleted_flg': False,
+        }
+
+    # MARK: - Media overrides
+    def get_image_info(self, image_id: int, token: str) -> Tuple[requests.Response, Dict[str, Any]]:
+        """
+        Получение инфы о изображении
+        """
+        return self._mock_token_handler(token)
+
+    def get_typed_images(self, object_type: MediaRequester.IMAGE_OBJ_TYPES, object_id: int, token: str) -> \
+            Tuple[requests.Response, List[Dict[str, Any]]]:
+        """
+        Получение инфы об изображениях определенного типа
+        """
+        return self._mock_token_handler(token)
+
+    def create_image(self, object_type: MediaRequester.IMAGE_OBJ_TYPES, object_id: int, created_by: int, file_data,
+                     filename: str, token: str) -> Tuple[requests.Response, Dict[str, Any]]:
+        """
+        Создание изображения
+        """
+        raise NotImplementedError('Пока не заимплементил -- думаю что и не понадобится')

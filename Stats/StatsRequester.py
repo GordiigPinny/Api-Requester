e1b1fd7dc0396a824189961ac086850c8f9859e1
Gordiig <gordiig@gmail.com>
2020-04-08 19:57:49 +0300

    Finished with StatsRequester.py

--- Stats/StatsRequester.py
+++ Stats/StatsRequester.py
@@ -1,3 +1,17 @@
+from django.conf import settings
 from ._StatsRequester import StatsRequester as __s
+from ._MockStatsRequester import MockStatsRequester as __m
 
-StatsRequester = __s
+
+__tst = settings.TESTING
+try:
+    if __tst:
+        __art = settings.ALLOW_REQUESTS_TEST
+        StatsRequester = __s if __art else __m
+    else:
+        __ar = settings.ALLOW_REQUESTS
+        StatsRequester = __s if __ar else __m
+except AttributeError:
+    StatsRequester = __s if not __tst else __m
+
+

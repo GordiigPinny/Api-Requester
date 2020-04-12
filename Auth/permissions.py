ca6bf1e2408da2d6be4c02b79bc8a6e174ad5764
Gordiig <gordiig@gmail.com>
2020-04-12 12:43:25 +0300

    Fixed permissions

--- Auth/permissions.py
+++ Auth/permissions.py
@@ -1,6 +1,7 @@
 from django.conf import settings
-from ._permissions import IsAuthenticated as __I, IsModerator as __M, IsSuperuser as __S
-from ._mock_permissions import IsAuthenticated as __mI, IsModerator as __mM, IsSuperuser as __mS
+from ._permissions import IsAuthenticated as __I, IsModerator as __M, IsSuperuser as __S, IsAppTokenCorrect as __APP
+from ._mock_permissions import IsAuthenticated as __mI, IsModerator as __mM, IsSuperuser as __mS, \
+    IsAppTokenCorrect as __mAPP
 
 
 __tst = settings.TESTING
@@ -10,12 +11,15 @@ try:
         IsAuthenticated = __I if __art else __mI
         IsModerator = __M if __art else __mM
         IsSuperuser = __S if __art else __mS
+        IsAppTokenCorrect = __APP if __art else __mAPP
     else:
         __ar = settings.ALLOW_REQUESTS
         IsAuthenticated = __I if __ar else __mI
         IsModerator = __M if __ar else __mM
         IsSuperuser = __S if __ar else __mS
+        IsAppTokenCorrect = __APP if __ar else __mAPP
 except AttributeError:
     IsAuthenticated = __I if __tst else __mI
     IsModerator = __M if __tst else __mM
     IsSuperuser = __S if __tst else __mS
+    IsAppTokenCorrect = __APP if __tst else __mAPP

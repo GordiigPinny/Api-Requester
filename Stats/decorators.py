551128f785749308548c99b1d2b6900f376fa673
Gordiig <gordiig@gmail.com>
2020-04-12 18:03:44 +0300

    Fixed decorator

--- Stats/decorators.py
+++ Stats/decorators.py
@@ -5,18 +5,20 @@ from ..utils import get_token_from_request
 from ..exceptions import BaseApiRequestError
 
 
-def collect_request_stats_decorator(func, app_id, app_secret, another_stats_funcs=[]):
-    def wrappe(self: CollectStatsMixin, request, *args, **kwargs):
-        try:
-            app_tokens = AuthRequester().app_get_token(app_id, app_secret, get_token_from_request(request))
-        except BaseApiRequestError:
-            return func(self, request, *args, **kwargs)
-        start_time = timeit.default_timer()
-        response = func(self, request, *args, **kwargs)
-        process_time = start_time - timeit.default_timer()
-        self.collect_request_stats(app_token=app_tokens['access'], process_time=process_time, endpoint=app_id,
-                                   request=request, response=response)
-        for stat_func, func_kwargs in another_stats_funcs:
-            stat_func(self, **func_kwargs)
-        return response
-    return wrappe
+def collect_request_stats_decorator(app_id, app_secret, another_stats_funcs=[]):
+    def decorator(func):
+        def wrappe(self: CollectStatsMixin, request, *args, **kwargs):
+            try:
+                app_tokens = AuthRequester().app_get_token(app_id, app_secret, get_token_from_request(request))
+            except BaseApiRequestError:
+                return func(self, request, *args, **kwargs)
+            start_time = timeit.default_timer()
+            response = func(self, request, *args, **kwargs)
+            process_time = start_time - timeit.default_timer()
+            self.collect_request_stats(app_token=app_tokens['access'], process_time=process_time, endpoint=app_id,
+                                       request=request, response=response)
+            for stat_func, func_kwargs in another_stats_funcs:
+                stat_func(self, **func_kwargs)
+            return response
+        return wrappe
+    return decorator

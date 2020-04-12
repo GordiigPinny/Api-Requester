7714b03f061d873e8ec6e3cd36362ca607a9c2df
Gordiig <gordiig@gmail.com>
2020-04-12 17:58:01 +0300

    Add decorator for collect stats

---
+++ Stats/decorators.py
@@ -0,0 +1,22 @@
+import timeit
+from .mixins import CollectStatsMixin
+from ..Auth.AuthRequester import AuthRequester
+from ..utils import get_token_from_request
+from ..exceptions import BaseApiRequestError
+
+
+def collect_request_stats_decorator(func, app_id, app_secret, another_stats_funcs=[]):
+    def wrappe(self: CollectStatsMixin, request, *args, **kwargs):
+        try:
+            app_tokens = AuthRequester().app_get_token(app_id, app_secret, get_token_from_request(request))
+        except BaseApiRequestError:
+            return func(self, request, *args, **kwargs)
+        start_time = timeit.default_timer()
+        response = func(self, request, *args, **kwargs)
+        process_time = start_time - timeit.default_timer()
+        self.collect_request_stats(app_token=app_tokens['access'], process_time=process_time, endpoint=app_id,
+                                   request=request, response=response)
+        for stat_func, func_kwargs in another_stats_funcs:
+            stat_func(self, **func_kwargs)
+        return response
+    return wrappe

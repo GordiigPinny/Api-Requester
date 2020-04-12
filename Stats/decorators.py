03f9b04f4224aa42d1bbfcbb86474c1e86fd58a4
Gordiig <gordiig@gmail.com>
2020-04-12 18:09:47 +0300

    Fixed call

--- Stats/decorators.py
+++ Stats/decorators.py
@@ -2,7 +2,6 @@ import timeit
 from django.conf import settings
 from .mixins import CollectStatsMixin
 from ..Auth.AuthRequester import AuthRequester
-from ..utils import get_token_from_request
 from ..exceptions import BaseApiRequestError
 
 
@@ -10,7 +9,7 @@ def collect_request_stats_decorator(app_id=settings.APP_ID, app_secret=settings.
     def decorator(func):
         def wrappe(self: CollectStatsMixin, request, *args, **kwargs):
             try:
-                app_tokens = AuthRequester().app_get_token(app_id, app_secret, get_token_from_request(request))
+                app_tokens = AuthRequester().app_get_token(app_id, app_secret)
             except BaseApiRequestError:
                 return func(self, request, *args, **kwargs)
             start_time = timeit.default_timer()

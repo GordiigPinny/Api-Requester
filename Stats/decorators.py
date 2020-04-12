ebec4466be68dace59782a8b1fff04e814f7b30d
Gordiig <gordiig@gmail.com>
2020-04-12 20:58:15 +0300

    Fixed some

--- Stats/decorators.py
+++ Stats/decorators.py
@@ -23,11 +23,15 @@ def collect_request_stats_decorator(app_id=settings.APP_ID, app_secret=settings.
 
             start_time = timeit.default_timer()
             response = func(self, request, *args, **kwargs)
+            additional_kwargs_for_stats_funcs = []
+            if isinstance(response, tuple):
+                additional_kwargs_for_stats_funcs = response[1]
+                response = response[0]
             process_time = timeit.default_timer() - start_time
             self.collect_request_stats(app_token=token, process_time=process_time, endpoint=app_id,
                                        request=request, response=response)
 
-            for stat_func, func_kwargs in zip(another_stats_funcs, self.additional_kwargs_for_stats_funcs):
+            for stat_func, func_kwargs in zip(another_stats_funcs, additional_kwargs_for_stats_funcs):
                 func_kwargs['app_token'] = token
                 stat_func(self, **func_kwargs)
             return response

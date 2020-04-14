9474a98c3a7eadfbad9a4d95cbb6e4ec99057165
Gordiig <gordiig@gmail.com>
2020-04-14 14:40:10 +0300

    Fixed decorator

--- Stats/decorators.py
+++ Stats/decorators.py
@@ -1,12 +1,18 @@
 import timeit
 from django.conf import settings
+from ..Auth.AuthRequester import AuthRequester
+from ..utils import get_token_from_request
 from .mixins import CollectStatsMixin
 
 
 def collect_request_stats_decorator(app_id=settings.APP_ID, app_secret=settings.APP_SECRET, another_stats_funcs=[]):
     def decorator(func):
         def wrappe(self: CollectStatsMixin, request, *args, **kwargs):
-            token = self.app_access_token
+            try:
+                token = self.app_access_token
+            except AttributeError:
+                _, tokens = AuthRequester().app_get_token(app_id, app_secret, token=get_token_from_request(request))
+                token = tokens['access']
             start_time = timeit.default_timer()
             response = func(self, request, *args, **kwargs)
             additional_kwargs_for_stats_funcs = []
